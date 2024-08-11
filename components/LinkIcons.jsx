ff81702509bb36b75bbe9223970b76a20fc4ad56
danydodson <danydodson@gmail.com>
2024-08-11 00:28:03 -0500

    Update Twitch link in LinkIcons component

--- components/LinkIcons.jsx
+++ components/LinkIcons.jsx
@@ -3,9 +3,9 @@ import Link from 'next/link'
 import { FaGithubAlt, FaTwitch, FaTwitterSquare, FaTiktok, FaPenSquare, FaEnvelopeOpenText } from 'react-icons/fa'
 
 const gitHub = 'https://github.com/danydodson'
-const twitch = 'https://www.twitch.tv/danydodson'
-const twitter = 'https://twitter.com/danydodson'
-const tiktok = 'https://www.tiktok.com/@dinydatsun'
+const twitch = 'https://www.twitch.tv/dany_spatula_hands'
+const twitter = 'https://twitter.com/dany_spatch'
+// const tiktok = 'https://www.tiktok.com/@dinydatsun'
 const portfolio = 'https://danydodson.dev/'
 const mail = 'mailto:dany@danydodson.dev'
 
@@ -14,7 +14,7 @@ export default function LinkIcons() {
     <main className='iconbox shadow-pop-br slide-in-top'>
       <Link className='iconbox-link' href={gitHub} target='_blank' rel='noopener noreferrer'><FaGithubAlt /></Link>
       <Link className='iconbox-link' href={twitter} target='_blank' rel='noopener noreferrer'><FaTwitterSquare /></Link>
-      <Link className='iconbox-link' href={tiktok} target='_blank' rel='noopener noreferrer'><FaTiktok /></Link>
+      {/* <Link className='iconbox-link' href={tiktok} target='_blank' rel='noopener noreferrer'><FaTiktok /></Link> */}
       <Link className='iconbox-link' href={twitch} target='_blank' rel='noopener noreferrer'><FaTwitch /></Link>
       <Link className='iconbox-link' href={portfolio} target='_blank' rel='noopener noreferrer'><FaPenSquare /></Link>
       <Link className='iconbox-link' href={mail} target='_blank' rel='noopener noreferrer'><FaEnvelopeOpenText /></Link>
